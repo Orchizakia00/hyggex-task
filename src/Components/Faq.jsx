134577e8d749286eb7e5f05e9d932bf18609a5a1
Orchizakia00 <zakiaislam290@gmail.com>
2024-01-16 18:23:25 +0600

    faq text edited

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -12,31 +12,31 @@ const Faq = () => {
         <div className="mb-80">
             <h2 className="font-bold text-5xl mb-12" style={textStyle}>FAQ</h2>
             <div className="w-[850px]">
-                <div className="collapse collapse-arrow mb-8" style={{border: '1px solid #217EEC'}}>
+                <div className="collapse collapse-arrow mb-8" style={{ border: '1px solid #217EEC' }}>
                     <input type="radio" name="my-accordion-2" />
                     <div className="collapse-title text-lg font-medium">
-                        Click to open this one and close others
+                        Can education flashcards be used for all age groups?
                     </div>
                     <div className="collapse-content">
-                        <p>hello</p>
+                        <p>answer</p>
                     </div>
                 </div>
-                <div className="collapse collapse-arrow mb-8" style={{border: '1px solid #217EEC'}}>
+                <div className="collapse collapse-arrow mb-8" style={{ border: '1px solid #217EEC' }}>
                     <input type="radio" name="my-accordion-2" />
                     <div className="collapse-title text-lg font-medium">
-                        Click to open this one and close others
+                        How do education flashcards work?
                     </div>
                     <div className="collapse-content">
-                        <p>hello</p>
+                        <p>answer</p>
                     </div>
                 </div>
-                <div className="collapse collapse-arrow mb-8" style={{border: '1px solid #217EEC'}}>
+                <div className="collapse collapse-arrow mb-8" style={{ border: '1px solid #217EEC' }}>
                     <input type="radio" name="my-accordion-2" />
                     <div className="collapse-title text-lg font-medium">
-                        Click to open this one and close others
+                        Can education flashcards be used for test preparation?
                     </div>
                     <div className="collapse-content">
-                        <p>hello</p>
+                        <p>answer</p>
                     </div>
                 </div>
             </div>
